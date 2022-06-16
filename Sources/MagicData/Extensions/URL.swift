35e6f1252ccc58c557de2ba7c81b96b60d841315
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-06-16 15:54:00 +0800

    fix

--- Sources/MagicData/Extensions/URL.swift
+++ Sources/MagicData/Extensions/URL.swift
@@ -1,5 +1,5 @@
 //
-//  URL+Path.swift
+//  URL.swift
 //  
 //
 //  Created by 朱浩宇 on 2022/6/15.
@@ -9,7 +9,7 @@ import Foundation
 
 extension URL {
     func universalPath() -> String {
-        if #available(macOS 13.0, *) {
+        if #available(macOS 13.0, iOS 16, *) {
             return self.path()
         } else {
             return self.path
@@ -17,7 +17,7 @@ extension URL {
     }
 
     func universalAppending(path: String) -> Self {
-        if #available(macOS 13.0, *) {
+        if #available(macOS 13.0, iOS 16, *) {
             return self.appending(path: path)
         } else {
             return self.appendingPathComponent(path)
